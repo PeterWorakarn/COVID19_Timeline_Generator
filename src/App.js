4bb66a37c1f0a6cf243a87f400e14e43b5c4426e
Peter Oun <57410426+PeterWorakarn@users.noreply.github.com>
2021-06-25 23:53:42 +0700

    Update overlap element

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <AppProvider>
       <div className="min-w-full min-h-full bg-brand-background relative">
-        <div className="hidden md:absolute md:top-10 md:right-48 md:z-0"><div className="sphere-2"></div></div>
+        <div className="hidden md:block md:absolute md:top-10 md:right-48 md:z-0"><div className="sphere-2"></div></div>
         <div className="md:container mx-auto max-w-sm md:max-w-3xl font-fontMain pt-24">
           <Transition appear={true} show={true} enter="transition-opacity duration-2500" enterFrom="opacity-0" enterTo="opacity-100" leave="transition-opacity duration-150" leaveFrom="opacity-100" leaveTo="opacity-0">
             <Header />
@@ -22,7 +22,7 @@ function App() {
           <Footer />
           </Transition>
         </div>
-        <div className="hidden md:absolute  md:bottom-10 md:left-40 md:z-0"><div className="sphere-1"></div></div>
+        <div className="hidden md:block md:absolute  md:bottom-10 md:left-40 md:z-0"><div className="sphere-1"></div></div>
       </div>
     </AppProvider>
   );
